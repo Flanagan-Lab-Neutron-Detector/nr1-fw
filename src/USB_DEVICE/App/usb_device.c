87265542a4eb2698d878a708afbb86195334ddbf
Aidan Medcalf <aidanmedcalf@gmail.com>
2023-04-17 17:32:49 -0500

    Decreased OTG HS IRQ subgroup priority from 0 to 1

--- src/USB_DEVICE/App/usb_device.c
+++ src/USB_DEVICE/App/usb_device.c
@@ -88,6 +88,8 @@ void MX_USB_DEVICE_Init(void)
   /* USER CODE BEGIN USB_DEVICE_Init_PostTreatment */
   HAL_PWREx_EnableUSBVoltageDetector();
 
+  HAL_NVIC_SetPriority(OTG_HS_IRQn, 0, 1);
+
   /* USER CODE END USB_DEVICE_Init_PostTreatment */
 }
 
