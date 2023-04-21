78112f8d0d9ef38be9429d3f1a12c0abb80dfb31
Aidan Medcalf <aidanmedcalf@gmail.com>
2023-04-21 16:01:46 -0500

    Slow down TIM13 interrupt

--- src/main.c
+++ src/main.c
@@ -328,7 +328,7 @@ static void MX_TIM13_Init(void)
 	TIM_ClockConfigTypeDef sClockSourceConfig = {0};
 
 	htim13.Instance = TIM13;
-	htim13.Init.Prescaler = 0;
+	htim13.Init.Prescaler = 128;
 	htim13.Init.CounterMode = TIM_COUNTERMODE_UP;
 	htim13.Init.Period = 65535;
 	htim13.Init.ClockDivision = TIM_CLOCKDIVISION_DIV4;
